4727605bf638ec0535c1f05d77e5ab6dc8b3d0cd
mandydinh <dinhquynhnhu14@gmail.com>
2019-12-28 01:31:58 +0700

    delete info at main.cpp

--- MainPro.cpp
+++ MainPro.cpp
@@ -1,7 +1,4 @@
 /*
-* Ho Ten: Dinh Thi Quynh Nhu
-* THUC HANH TOAN ROI RAC : TAO BANG CHAN TRI
-* DEADLINE: Friday, 26th, Apr, 2019
 * Compile and execute by terminal: 
 	+ Using: g++ [file .cpp] [option] [executed file]
 	+ After that, executes this file.
@@ -12,7 +9,8 @@
 	1. Finish read file "logicexpression.txt" and convert to Polish Notation
 	2. Finish generate binary table(Ex: 3 values -> 2^4=3 = 8 lines with value 0,1).
 	3. Finish calculate true table and write to "output.txt"
-	4. Not finish the TITLE of true table yet. 
+	4. Not finish the TITLE of true table yet.
+	5. Completely print the TITLE (updated Dec/2019) 
 */
 
 #include "TrueTable.cpp"
